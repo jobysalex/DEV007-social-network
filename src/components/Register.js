a113a11e5f72b2c7d69bea236ca03ad6d7e55187
Carmen Luna <lunacarmen22@gmail.com>
2023-06-12 10:58:33 -0500

    boton google

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,9 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../Firebase";
-import { crearUsuarioConCorreoYContraseña } from "../lib";
+// import { createUserWithEmailAndPassword } from "firebase/auth";
+// import { auth } from "../Firebase";
+import { crearUsuarioConCorreoYContraseña } from '../lib';
+
 export const Register = (onNavigate) => {
-  const HomeDiv = document.createElement("div");
+  const HomeDiv = document.createElement('div');
 
   const viewRegister = `
     <div><img src="./img/tarta.jpg" class="imgMain" alt="Imagen de Tarta - Cocinemos Juntos "></div>
@@ -13,37 +14,49 @@ export const Register = (onNavigate) => {
       <input type="email" class="email" id="email2" placeholder="ejemplo@gmail.com " />
       <label> Ingresa tu Contraseña </label>
       <input type="password" class="password" id="password2" placeholder="xxx" />
-    </section>
+      </section>
+      <button type="button" class="googleSignIn" id="googleButton">
+      <img src="img/LogoGoogle.png" alt="GoogleIcon" class="GoogleIcon">
+      <span id="registerGoogle">Registrate con Google</span>
+      </button>
     `;
   HomeDiv.innerHTML = viewRegister;
 
   // HomeDiv.textContent = 'Bienvenida al registro';
-  const buttonLogin = document.createElement("button");
-  buttonLogin.classList.add("buttonsPrincipals");
-  buttonLogin.textContent = "Registrarse";
+  const buttonLogin = document.createElement('button');
+  buttonLogin.classList.add('buttonsPrincipals');
+  buttonLogin.textContent = 'Registrarse';
   // buttonLogin.addEventListener('click', () => onNavigate('/login'));
   buttonLogin.onclick = function () {
-    const inputEmail = HomeDiv.querySelector("#email2");
-    const inputPassword = HomeDiv.querySelector("#password2");
-    console.log(inputEmail.value);
-    console.log(inputPassword.value);
+    const inputEmail = HomeDiv.querySelector('#email2');
+    const inputPassword = HomeDiv.querySelector('#password2');
+    // console.log(inputEmail.value);
+    // console.log(inputPassword.value);
     crearUsuarioConCorreoYContraseña(inputEmail.value, inputPassword.value)
       .then(() => {
-        onNavigate("/login");
+        onNavigate('/login');
       })
       .catch((err) => console.log(err));
   };
+
   HomeDiv.appendChild(buttonLogin);
 
-  const buttonHome = document.createElement("button");
-  buttonHome.classList.add("buttonsPrincipals");
-  buttonHome.textContent = "Regresar al home";
-  buttonHome.addEventListener("click", () => onNavigate("/")); //esto es para que vuelva al home
+  const buttoGoogle = document.createElement('button');
+  buttoGoogle.classList.add('googleSignIn');
+  buttoGoogle.textContent = 'Registrarse con Google';
+  HomeDiv.appendChild(buttoGoogle);
+
+
+  const buttonHome = document.createElement('button');
+  buttonHome.classList.add('buttonsPrincipals');
+  buttonHome.textContent = 'Regresar al home';
+  buttonHome.addEventListener('click', () => onNavigate('/')); // esto es para que vuelva al home
   HomeDiv.appendChild(buttonHome);
 
   return HomeDiv;
 };
 
-//const email = document.getElementById("email2").value;
-//const password = document.getElementById("password2").value; //con esto obtengo los valores email y password que fue ingresado
-//alert(email + password);
+// const email = document.getElementById("email2").value;
+// eslint-disable-next-line max-len
+// const password = document.getElementById("password2").value; //con esto obtengo los valores email y password que fue ingresado
+// alert(email + password);
