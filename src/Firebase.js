ae3881416ee5c6f627edbe13222eb40dc4870a88
Carmen Luna <lunacarmen22@gmail.com>
2023-06-16 12:18:10 -0500

    alertas en login

--- src/Firebase.js
+++ src/Firebase.js
@@ -8,14 +8,6 @@ import {
   signInWithPopup,
 } from 'firebase/auth';
 
-// import { gestFirestore } from 'firebase, firestore';
-
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyDSOGg0XwGEFGCZEwnyaMopX8zyrHPlk_A',
   authDomain: 'cocinemos-juntos.firebaseapp.com',
@@ -25,8 +17,6 @@ const firebaseConfig = {
   appId: '1:853852150690:web:bb9be5b357b688ee9830fb',
   measurementId: 'G-PRMN0GBXC5',
 };
-
-// const analytics = getAnalytics(app);
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 
@@ -36,7 +26,7 @@ export const auth = getAuth(app);
 
 // funcion registrarse secion con mail y password
 export function crearUsuarioConCorreoYContraseña(email, password) {
-  return createUserWithEmailAndPassword(auth, email, password);
+ return createUserWithEmailAndPassword(auth, email, password);
 }
 
 // funcion iniciar secion con mail y password
@@ -49,5 +39,3 @@ export function iniciarConGoogle() {
   const provider = new GoogleAuthProvider();
   return signInWithPopup(auth, provider);
 }
-
-// export const db = getFirestore(app);
